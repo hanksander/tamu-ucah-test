4900f0b6dd0892b6c5bfeb8a8dcf2fc786b16259
missle2 <108312727+missle2@users.noreply.github.com>
2025-09-23 15:45:12 -0500

    Moved to it's own folder

---
+++ mdao_blunted_cone/thermal.py
@@ -0,0 +1,26 @@
+class Thermal(co.ODESystem):
+    """
+    Simple thermal model with convective heating.
+    Parameters
+    ----------
+    q_dot : convective heat rate [W/m^2]
+    A_ref : reference area [m^2]
+    m     : mass [kg]
+    cp    : specific heat [J/(kg*K)]
+    T0    : initial temperature [K]
+    Outputs
+    -------
+    T : temperature [K]
+    ----
+    TODO - generalize to multiple materials with different thermal properties"""
+    q_dot = parameter()  # convective heat rate, W/m^2
+    A_ref = parameter()  # reference area, m^2
+    m = parameter()      # mass, kg
+    cp = parameter()     # specific heat, J/(kg*K)
+    T0 = parameter()     # initial temperature, K
+
+    T = state()          # temperature, K
+
+    dot[T] = q_dot * A_ref / (m * cp)
+
+    initial[T] = T0
